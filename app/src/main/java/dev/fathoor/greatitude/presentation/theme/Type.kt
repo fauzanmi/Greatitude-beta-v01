59724cf04e28a265c99f1e73adce229246d9ad3f
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-06-12 08:44:45 +0700

    chore(app): init app resources

---
+++ app/src/main/java/dev/fathoor/greatitude/presentation/theme/Type.kt
@@ -0,0 +1,13 @@
+package dev.fathoor.greatitude.presentation.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import dev.fathoor.greatitude.R
+
+val FontPlusJakarta = FontFamily(
+    Font(R.font.plusjakarta_regular, FontWeight.Normal),
+    Font(R.font.plusjakarta_medium, FontWeight.Medium),
+    Font(R.font.plusjakarta_semibold, FontWeight.SemiBold),
+    Font(R.font.plusjakarta_bold, FontWeight.Bold),
+)
